afffd57853da78c0c322aae22821939471ea66f1
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-07-20 11:22:13 +0900

    스웨거 설정 추가

---
+++ src/main/kotlin/com/example/board/config/SwaggerConfig.kt
@@ -0,0 +1,68 @@
+package com.example.board.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RequestMethod
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.builders.ResponseMessageBuilder
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.service.ResponseMessage
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    private var version:String? = null
+    private var title:String? = null
+
+    @Bean
+    fun apiV1() : Docket? {
+        version = "V1"
+        title = "hyeok API $version"
+
+        var responseMessage = ArrayList<ResponseMessage>()
+        responseMessage.add(
+            ResponseMessageBuilder()
+            .code(200)
+            .message("OK")
+            .build())
+
+        responseMessage.add(
+            ResponseMessageBuilder()
+            .code(404)
+            .message("Not Found")
+            .build())
+
+        responseMessage.add(
+            ResponseMessageBuilder()
+            .code(500)
+            .message("Internal Server Error")
+            .build())
+
+        return Docket(DocumentationType.SWAGGER_2)
+            .groupName(version!!)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.example.swagger.controller"))
+            .paths(PathSelectors.ant("/v1/api/**"))
+            .build()
+            .apiInfo(apiInfo(title!!, version!!))
+            .globalResponseMessage(RequestMethod.GET, responseMessage)
+    }
+
+    private fun apiInfo(title:String, version:String) : ApiInfo {
+        return ApiInfo(
+            title,
+            "Swagger로 생성한 Api Docs",
+            version,
+            "www.example.swagger",
+            Contact("Contact Me", "www.example.com", "foo@example.com"),
+            "Licenses",
+            "www.example.com",
+            ArrayList()
+        )
+    }
+}
